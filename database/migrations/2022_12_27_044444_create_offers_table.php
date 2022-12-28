61f82014ceedaafb2ce6bb642753dde7519ef938
RyuAgusta <107595879+RyuAgusta@users.noreply.github.com>
2022-12-28 13:39:23 +0800

    Offer Tables

---
+++ database/migrations/2022_12_27_044444_create_offers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->increments('offerID');
+            $table->date('offerDate');
+            $table->string('remarks');
+            $table->boolean('offerStatus');
+            $table->integer('requestID');
+            $table->string('username');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
